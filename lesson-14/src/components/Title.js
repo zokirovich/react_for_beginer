03bcdc7fe180fb5a4e9eb2c5a64750bd4f89fa84
zokirovich <samandarovsakir@gmail.com>
2026-04-14 00:17:50 +0500

    lesson_14

---
+++ lesson-14/src/components/Title.js
@@ -0,0 +1,13 @@
+
+const Title = ({title,subText}) => {
+	// console.log(props) 
+	return (
+		<div>
+			<h1 className='title'>{title}</h1>
+			<p className='subTitle'>{subText}</p>
+		</div>
+	)
+
+}
+
+export default Title 
