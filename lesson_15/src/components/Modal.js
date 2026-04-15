c22d1c8bdc73f46b86915fcd21200e922016f81c
zokirovich <samandarovsakir@gmail.com>
2026-04-15 23:25:09 +0500

    ReactDOM_createPortal

--- lesson_15/src/components/Modal.js
+++ lesson_15/src/components/Modal.js
@@ -1,9 +1,18 @@
 import './Modal.css'
-
 function Modal({children,closeModal}) {
 
 	// console.log(props)
 	// console.log(props)
+	// return ReactDOM.createPortal((
+	// 	<div className='modal-backdrop'> 
+	// 		<div className='modal'>
+	// 		   {children}
+	// 			 <br />
+	// 			 <br />
+	// 			 <button onClick={closeModal}>Remove click</button>
+	// 		</div>
+	// 	</div>
+	// ),document.body)
 	return (
 		<div className='modal-backdrop'> 
 			<div className='modal'>
